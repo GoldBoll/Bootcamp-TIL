d9265688007588a4c99934447cebbb96a1ac1695
Lupang <goldball1012@gmail.com>
2026-03-05 09:35:47 +0900

    CodeKata

---
+++ CodeingTest/CodingTest/CodeKata.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include <string>
+#include <vector>
+using namespace std;
+
+#ifdef RUN_PROGRAMMERS_CodeKata_01
+int solution(int num1, int num2) {
+    
+    return num1 - num2;
+}
+#endif
+
+
+#ifdef RUN_PROGRAMMERS_CodeKata_02
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    
+    return num1*num2;
+}
+
+#endif
