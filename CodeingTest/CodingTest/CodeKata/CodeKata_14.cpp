0ad922d92ead747b2940bb98bedb06f06fedb02a
Lupang <goldball1012@gmail.com>
2026-04-14 14:58:55 +0900

    Create CodeKata_14.cpp

---
+++ CodeingTest/CodingTest/CodeKata/CodeKata_14.cpp
@@ -0,0 +1,22 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/12935
+
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    vector<int> answer;
+    if(arr.empty() || arr.size() == 1)
+    {
+        return {-1};
+    }
+    int min = *min_element(arr.begin(), arr.end());
+    
+    for(auto& n : arr)
+    {
+        if(n != min) answer.push_back(n);
+    }
+    
+    return answer;
+}
