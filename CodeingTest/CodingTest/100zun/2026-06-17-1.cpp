b5df47d00491afba30ea2402899d1efdabd2e8f2
Lupang <goldball1012@gmail.com>
2026-06-17 21:43:58 +0900

    algo: 알고리즘 시험 3문제 풀이(회의실 그리디·계단 DP·MT장보기 백트래킹) + TIL 발행

---
+++ CodeingTest/CodingTest/100zun/2026-06-17-1.cpp
@@ -0,0 +1,27 @@
+// 백준 ____ -
+// https://www.acmicpc.net/problem/
+
+// 문제 설명
+//
+
+// 제약 조건
+//
+
+// 예제
+// 입력            출력
+//
+
+// 접근
+// -
+// - 시간 복잡도 O()
+
+#include <iostream>
+#include <string>
+using namespace std;
+#define endl '\n'
+
+int main()
+{
+
+    return 0;
+}
